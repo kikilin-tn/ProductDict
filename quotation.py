a7af3cf125c6b39e76be79c5ec59ed8fabe6ab78
Kiki Lin <mikey2862@gmail.com>
2019-10-24 10:03:42 +0800

    add new quoation dictionary

---
+++ quotation.py
@@ -0,0 +1,32 @@
+
+#production = []
+p0 = {
+    'name':'DFL7161',
+    'price':'200'
+    }
+#production.append(p0)
+
+p1 = {
+    'name':'Grinder wheel',
+    'price':'50'
+    }
+#production.append(p1)
+production = [p0, p1]
+print('The price of the ' + production[0]['name'] + ' is '+ production[0]['price'])
+print('The price of the ' + production[1]['name'] + ' is '+ production[1]['price'])
+print('====================')
+print(production)
+
+"""
+person = {
+    'first_name': 'eric',
+    'last_name': 'matthes',
+    'age': 43,
+    'city': 'sitka',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+"""
