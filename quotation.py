91c88916a34a0caa474144bba152601abcb7d7a0
kikilin-tn <55426142+kikilin-tn@users.noreply.github.com>
2019-10-24 10:04:25 +0800

    Update quotation.py

--- quotation.py
+++ quotation.py
@@ -16,17 +16,3 @@ print('The price of the ' + production[0]['name'] + ' is '+ production[0]['price
 print('The price of the ' + production[1]['name'] + ' is '+ production[1]['price'])
 print('====================')
 print(production)
-
-"""
-person = {
-    'first_name': 'eric',
-    'last_name': 'matthes',
-    'age': 43,
-    'city': 'sitka',
-    }
-
-print(person['first_name'])
-print(person['last_name'])
-print(person['age'])
-print(person['city'])
-"""
